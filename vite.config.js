11e54cd8dbad2087e5645bca1ce969bf0612c77f
Raghunandan das <90844992+RAGHUNANDANLOG@users.noreply.github.com>
2026-01-09 03:29:26 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:6000',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: 'http://localhost:6000',
-        changeOrigin: true
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        },
+        '/uploads': {
+          target: env.VITE_API_URL,
+          changeOrigin: true
+        }
       }
     }
-  }
-});
-
-
-
+  });
+};
